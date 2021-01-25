5a74fa977b3fdcf0ec6d3fe40c4f029461da8994
sümeyra sağır <sumeyrasagir2017@outlook.com>
2021-01-25 21:09:15 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<KeyT, ValueT>
+    {
+        KeyT[] keyTs;
+        ValueT[] valueTs;
+
+        public MyDictionary()
+        {
+            keyTs = new KeyT[0];
+            valueTs = new ValueT[0];
+        }
+
+        public void Add(KeyT keyT , ValueT valueT)
+        {
+            KeyT[] tempKeyT = keyTs;
+            ValueT[] tempValueT = valueTs;
+
+            keyTs = new KeyT[keyTs.Length + 1];
+            valueTs = new ValueT[valueTs.Length + 1];
+
+            for (int i = 0; i < tempKeyT.Length; i++)
+            {
+                keyTs[i] = tempKeyT[i];
+                valueTs[i] = tempValueT[i];
+            }
+
+            keyTs[keyTs.Length - 1] = keyT;
+            valueTs[valueTs.Length - 1] = valueT;
+
+        }
+
+        public void Show()
+        {
+            for (int i = 0; i < keyTs.Length; i++)
+            {
+                Console.WriteLine(keyTs[i] + " = " + valueTs[i]);
+            }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Elma", "Apple");
+            myDictionary.Add("Karpuz", "Watermelon");
+            myDictionary.Add("Portakal", "Orange");
+            myDictionary.Add("Kayısı", "Apricot");
+            myDictionary.Show();
+        }
+    }
+}
